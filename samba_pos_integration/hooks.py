cc95281e3386d66dbf122b930937bc37467f6e12
Nickesh <nick9822@gmail.com>
2019-08-08 14:01:16 +0000

    Renaming brought methods

--- samba_pos_integration/hooks.py
+++ samba_pos_integration/hooks.py
@@ -92,19 +92,19 @@ app_license = "MIT"
 
 scheduler_events = {
 	# "all": [
-	# 	"triangle_llc.tasks.all"
+	# 	"samba_pos_integration.tasks.all"
 	# ],
 	"daily": [
         "samba_pos_integration.samba_pos_integration.samba_utilities.submit_samba_invoices"
 	]
 	# "hourly": [
-	# 	"triangle_llc.tasks.hourly"
+	# 	"samba_pos_integration.tasks.hourly"
 	# ],
 	# "weekly": [
-	# 	"triangle_llc.tasks.weekly"
+	# 	"samba_pos_integration.tasks.weekly"
 	# ]
 	# "monthly": [
-	# 	"triangle_llc.tasks.monthly"
+	# 	"samba_pos_integration.tasks.monthly"
 	# ]
 }
 
